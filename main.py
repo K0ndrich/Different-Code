171e1888724f0c53b3e6991ab96c1851eaa2a4da
K0ndrich <123kondrich@gmail.com>
2024-02-09 13:57:48 +0200

    was added paterns

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+# -----  Патерн Singleton  -----------------------------------------------------------------------------------------
+class MySigleton:
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, "instance"):
+            cls.instance = super(MySigleton, cls).__new__(cls)
+        return cls.instance
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+# -----  Патерн Моносостояние  --------------------------------------------------------------------------------------
+class MyMonoCondition:
+    __data = {"name": "", "age": 0, "city": ""}
+
+    def __init__ (self):
+        self.__dict__= self.__data
+
+
 # -----  Декораторы  ------------------------------------------------------------------------------------------------
-class Integer:
+class MyDecorator:
     def __set_name__(self, owner, name):
         self.name = "_" + name
 
@@ -10,14 +30,10 @@ class Integer:
         instance.__dict__[self.name] = value
 
 
-class Point3D:
-    x = Integer()
-    y = Integer()
+class OtherMyDecorator:
+    x = MyDecorator()
+    y = MyDecorator()
 
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
-
-a = Point3D(1, 2)
-print(a.x)
