5183933fb40e5e5c4ea073dd832607fdf3d5c076
K0ndrich <123kondrich@gmail.com>
2024-02-09 14:24:27 +0200

    edited desriptors

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ class MySigleton:
 class MyMonoCondition:
     __data = {"name": "", "age": 0, "city": ""}
 
-    def __init__ (self):
-        self.__dict__= self.__data
+    def __init__(self):
+        self.__dict__ = self.__data
 
 
 # -----   Дескрипторы   ------------------------------------------------------------------------------------------------
@@ -24,10 +24,10 @@ class MyDescriptor:
         self.name = "_" + name
 
     def __get__(self, instance, owner):
-        return instance.__dict__[self.name]
+        return instance.__dict__[self.name]  # или  return getattr( instance , self.name)
 
     def __set__(self, instance, value):
-        instance.__dict__[self.name] = value
+        instance.__dict__[self.name] = value  # или  setattr( instance, self.name , value)
 
 
 class OtherMyDescriptor:
@@ -37,3 +37,5 @@ class OtherMyDescriptor:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+
