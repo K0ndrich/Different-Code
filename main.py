ccdb995921386b406987a60e259a2c9bc360d8b0
K0ndrich <123kondrich@gmail.com>
2024-02-13 21:36:12 +0200

    functions

--- main.py
+++ main.py
@@ -18,6 +18,45 @@ class MyMonoCondition:
         self.__dict__ = self.__data
 
 
+# ----- Call Back Функции ------------------------------------------------------------------------------------------
+
+
+def my_fn1(func):
+
+    func()
+
+
+def my_call_back_func():  # my_fn2 - ето call back функция.
+    pass  # Ета функция передаеться внутрь другой функции, как аргумент
+
+
+my_fn1(my_call_back_func)
+
+# ----- Функции Замыкания -----------------------------------------------------------------------------------------
+
+
+def my_fn1():
+
+    def my_lock_func():  # my_fn2 - ето функция замыкания.
+        pass  # Ета функция реальзована внутри другой функции и возвращаеться из внешней функции
+
+    return my_lock_func
+
+
+# ----- Декораторы Функций -------------------------------------------------------------------------------------------------
+
+
+def my_decorator_func(func):
+    def wrapper(value):
+        func(value)
+
+    return wrapper
+
+@my_decorator_func
+def my_fn():
+    pass
+
+
 # -----   Дескрипторы   ------------------------------------------------------------------------------------------------
 class MyDescriptor:
     def __set_name__(self, owner, name):
@@ -29,7 +68,7 @@ class MyDescriptor:
         ]  # или  return getattr( instance , self.name)
 
     def __set__(self, instance, value):
-        instance.__dict__[self.name] = (
+        instance.__dict__[self.name]= (
             value  # или  setattr( instance, self.name , value)
         )
 
@@ -45,5 +84,3 @@ class OtherMyDescriptor:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
-
